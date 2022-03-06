f139d77befebde7ae6b11c3d8f8e4ca27e5db55c
ali-susanto <alisusanto.main@gmail.com>
2022-03-06 17:47:58 +0700

    update code diagonalNumber.dart

---
+++ bin/diagonalNumber.dart
@@ -0,0 +1,20 @@
+import 'dart:io';
+
+void numberDiagonal() {
+  stdout.write('Input = ');
+  int nilai = int.parse(stdin.readLineSync()!);
+  for (var i = 0; i < nilai; i++) {
+    for (var j = 0; j < nilai; j++) {
+      if (i == j) {
+        stdout.write(i + 1);
+      } else {
+        stdout.write(' ');
+      }
+    }
+    print('');
+  }
+}
+
+void main(List<String> args) {
+  numberDiagonal();
+}
